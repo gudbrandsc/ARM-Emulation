521b952dafbfc8d3813367877f5421d0bac2233e
gudbrandsc <gudbrand.schistad@gmail.com>
2018-04-04 19:06:11 +0000

    Removed commented out lines

--- fib_iter_s.s
+++ fib_iter_s.s
@@ -2,27 +2,25 @@
 	.func fib_iter_s
 fib_iter_s:
 	cmp r0, #1 // if(n > 1)
-//	bls set_one //if not
-//	push {r4, r5}
+	bls set_one //if not
+	sub sp, sp, #8
+	str r4, [sp, #0]
+	str r5, [sp, #4]
 	mov r1, #0 // int f1
 	mov r2, #1 // int f2
 	mov r3, #1 // int i
 	mov r4, #0 // int res
-
 loop:
-
 	cmp r3, r0 //if i == n
 	bge done // if true end
-
 	add r4, r2, r1  // sum = f1 + f2
 	add r3, r3, #1 // i = i + 1
 	mov r1, r2 // f1 = f2
 	mov r2, r4 // f2 = res
-
 	b loop // loop back
 done:
 	mov r0, r4 // mov fib value to return register
-//	pop {r4, r5}
+	add sp, sp, #8
 	b end // return
 set_one:
 	mov r0, #1 // set return value as 1 
--- find_max_s.s
+++ find_max_s.s
@@ -13,7 +13,7 @@ loop:
 	lsl r4, r2, #2 //R4 = i + 1
 	ldr r5, [r0, r4] // R5 = array[i]
 	add r2, r2, #1 // i= i + 1
-	cmp r5, r3 // if(array[i] >= res)
+	cmp r5, r3 // if(array[i] >= max)
 	bge set_largest // if(array[i) >= res -> if true
 	b loop //Repeat loop
 set_largest:
